f24367dc1b4e260e39b7198949d02d34a8e40279
minhlip <minhlips2002@gmail.com>
2024-02-05 15:54:29 +0700

    Setup Base

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import KanbanBoard from './components/KanbanBoard';
+
+export default function App() {
+  return (
+    <div>
+      <KanbanBoard />
+    </div>
+  );
+}
---
+++ src/components/KanbanBoard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function KanbanBoard() {
+  return (
+    <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]"></div>
+  );
+}
+
+export default KanbanBoard;
